4f05469616ba5fd85a6349d5944002365048ed6c
Henny <giovanna.henny@capgemini.com>
2022-01-12 16:49:40 -0500

    test

--- src/App.js
+++ src/App.js
@@ -4,45 +4,45 @@ import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      monsters: [ ],
+      monsters: [],
       searchField: ''
     };
 
     // this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}));
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
-  handleChange =(e) => {
-    this.setState({searchField: e.target.value});
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
   }
-
-render(){
-  const { monsters, searchField } = this.state;
-  const filteredMonsters = monsters.filter(monster =>
+  //test
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase()));
 
-  return (
-    <div className="App">
-      <h1>Cats Rolodex</h1>
-      <SearchBox 
-        placeholder='search cats'
-        handleChange={this.handleChange} />
+    return (
+      <div className="App">
+        <h1>Cats Rolodex</h1>
+        <SearchBox
+          placeholder='search cats'
+          handleChange={this.handleChange} />
 
-    <CardList monsters ={ filteredMonsters } />
-      
-    </div>
-  );
-}  
-  
+        <CardList monsters={filteredMonsters} />
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
